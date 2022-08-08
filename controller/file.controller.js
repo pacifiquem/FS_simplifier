061e6ffca5d397e4aafcdbe2bede5e0de52088f3
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-08-08 19:31:49 +0200

    finished /file

--- controller/file.controller.js
+++ controller/file.controller.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const crypto = require('crypto');
 const { FileSchema } = require('../model/file.model');
 const hash = require('../utils/hash.utils');
+const genIdentifier = require('../middleware/identifierGen.middleware');
 
 exports.home = ( async(req, res, next) => {
     res.render('home');
@@ -9,30 +10,30 @@ exports.home = ( async(req, res, next) => {
 
 
 exports.file = async(req, res, next) => {
+    
     try {
 
-        if(req.file && req.body.password) {
+        if(req.file || req.body.password) {
             
             const { originalname, path } = req.file;
-            const password = await hash(req.body.password);
-            let identifier = crypto.createHmac('sha256', process.env.CRYPTO_SECRET).update(originalname).digest('hex');
-
-            
+            const password = await hash(req.body.password);;
 
             const file = await FileSchema.create({
                 fileOrginalName: originalname,
                 file_path: path, 
                 password,
-                identifier: await hash(identifier)
+                identifier: await hash(req.identifier)
             });
 
-            console.log(identifier);
-            res.redirect(`/${identifier}`);
-            }
+            res.redirect(`/file/${req.identifier}`);
+
+        }
 
     } catch (error) {
+
         console.log(error)
         res.send('sorry');
+
     }
 
 };
--- router/file.router.js
+++ router/file.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const genIdentifier = require('../middleware/identifierGen.middleware')
 
 
 const upload = multer({dest: 'uploads/'});
@@ -8,6 +9,6 @@ const upload = multer({dest: 'uploads/'});
 const { home, file } = require('../controller/file.controller');
 
 router.route('/').get(home);
-router.route('/file').post( upload.single("uploadedFile") , file );
+router.route('/file').post( genIdentifier, upload.single("uploadedFile") , file );
 
 module.exports = router
