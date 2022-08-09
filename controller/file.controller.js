9c043bc1f1528ee6a6277c3d03589e4514303823
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-08-09 20:24:49 +0200

    added download file api

--- controller/file.controller.js
+++ controller/file.controller.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 const crypto = require('crypto');
 const { FileSchema } = require('../model/file.model');
 const hash = require('../utils/hash.utils');
-const genIdentifier = require('../middleware/identifierGen.middleware');
+const cryptoHash = require('../utils/cryptoHash.utils');
 
 exports.home = ( async(req, res, next) => {
     res.render('home');
 });
 
 
-exports.file = async(req, res, next) => {
+exports.uploadFile = async(req, res, next) => {
     
     try {
 
-        if(req.file || req.body.password) {
+        if(req.file && req.body.password) {
             
             const { originalname, path } = req.file;
             const password = await hash(req.body.password);;
@@ -22,17 +22,36 @@ exports.file = async(req, res, next) => {
                 fileOrginalName: originalname,
                 file_path: path, 
                 password,
-                identifier: await hash(req.identifier)
+                identifier: cryptoHash(req.identifier)
             });
 
-            res.redirect(`/file/${req.identifier}`);
+            res.status(200).json({
+                success: true, 
+                data: file.identifier
+            });
 
         }
 
     } catch (error) {
 
-        console.log(error)
-        res.send('sorry');
+        next(error);
+
+    }
+
+};
+
+exports.downloadFile = async (req, res, next) => {
+
+    const file = await FileSchema.findOne({
+        identifier: cryptoHash(req.params.identifier)
+    });
+
+    if((file != null && file != undefined) && (file.file_path != null && file.file_path != undefined)) {
+
+        res.download(file.file_path);
+        file.downloadsCount++;
+
+        await file.save();
 
     }
 
