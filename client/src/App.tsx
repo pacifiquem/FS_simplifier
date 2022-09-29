8f7b9c34cc5df5c8cbaf7542520ad74714900269
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-29 21:35:05 +0200

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>initial frontend commit</p>
+    </div>
+  );
+}
+
+export default App;
