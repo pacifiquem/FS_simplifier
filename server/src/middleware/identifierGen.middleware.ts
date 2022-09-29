d7108c21be0ba43c98a32e9dde3b120c6b7aa548
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-29 21:02:03 +0200

    tsc init

---
+++ server/src/middleware/identifierGen.middleware.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+
+const identifierGenerator = (req:Request, res:Response, next:NextFunction) => {
+
+    let firstSecondsPart = `${Date.now()}`;
+    let firstSecondsPartArr = firstSecondsPart.split('').slice(10, 13).join('');
+
+    var secondsSecondsPart, secondSecondsPartArr;
+
+    let anotherPartArr = setTimeout(() => {
+
+        secondsSecondsPart = `${Date.now()}`;
+        secondSecondsPartArr = secondsSecondsPart.split('').slice(9, 13).join('');
+        
+        return secondSecondsPartArr;
+    }, 50);
+
+    const identifier = `${anotherPartArr}.fs.simplifier.${firstSecondsPartArr}`;
+    req.body.identifier = identifier;
+
+    next();
+}
+
+export default identifierGenerator;
