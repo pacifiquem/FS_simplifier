3b80f896458e98148c44e8d7acb2c344f47b8edb
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-29 21:02:19 +0200

    tsc init

---
+++ server/src/routes/file.router.ts
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const genIdentifier = require('../middleware/identifierGen.middleware')
+
+
+const upload = multer({dest: 'uploads/'});
+
+const { home,  uploadFile, downloadFile } = require('../controller/file.controller');
+
+router.route('/').get(home);
+router.route('/file').post( genIdentifier, upload.single("uploadedFile") , uploadFile );
+router.route('/file/download/:identifier').post(downloadFile).get(downloadFile);
+
+module.exports = router;
