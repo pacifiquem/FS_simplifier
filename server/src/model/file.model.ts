693d2b758001d510d1329da195825b99a9a46647
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-29 21:02:10 +0200

    tsc init

---
+++ server/src/model/file.model.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+
+const FileSchema =new mongoose.Schema({
+    
+    fileOrginalName: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 200
+    },
+
+    file_path: {
+        type: String,
+        required: true,
+        unique: true, 
+    },
+
+    password: {
+        type: String, 
+        required: true,
+        minLength: [6, "password must be atleast 6 characters long"],
+        maxLength: ['password can\'t be above 12 characters long'] 
+    },
+
+    identifier: {
+        type: String, 
+        required: true, 
+        unique: true
+    },
+
+    downloadsCount: {
+        type: Number,
+        default: 0
+    },
+    
+    sharedAt: {
+        type: Date,
+        default: Date.now()
+    }
+    
+});
+
+
+export const FileModel = mongoose.model('file', FileSchema);
