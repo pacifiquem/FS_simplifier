34b25b3d9f026db925aa1a4f91d47d5cd687e443
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-04 19:17:53 +0200

    added nav

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavBar from '../components/navBar';
 
 const homePage:React.FC = () => {
     return (
         <>
+            <NavBar />
         </>
     );
 }
