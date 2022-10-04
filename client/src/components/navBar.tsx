ff1ed4164d0f716550f17c358ecb7f34b93bb24e
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-04 19:17:37 +0200

    components added to hold proj's components

---
+++ client/src/components/navBar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import '../assets/styles/styles.css';
+
+const NavBar:React.FC = () => {
+
+    return (
+        <>
+            <div className="nav">
+
+                <h1 className="logo">FS.simplifier</h1>
+                <div className="links">
+                    <a href="/download-counter">Download Counter</a>
+                    <a href="#aboutus">AboutUs</a>
+                    <a href="/contactus">ContactUs</a>
+                    <a href=""></a>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default NavBar;
