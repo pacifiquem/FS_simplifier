833f0d6de095953d9ad92161e012725560c4ab1c
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-04 19:18:15 +0200

    routing funcitonalities added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import HomePage from './pages/home';
+
+
 function App() {
   return (
     <div className="App">
-      <p>initial frontend commit</p>
+      <BrowserRouter>
+          <Routes>
+            <Route index element={<HomePage/>} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
