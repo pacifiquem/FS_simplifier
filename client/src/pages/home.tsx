4923aa96bd715acbe911604bf32764508ba13248
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-04 22:04:18 +0200

    rendered homeform component

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../components/navBar';
+import HomeForm from '../components/homeform';
 
 const homePage:React.FC = () => {
     return (
         <>
             <NavBar />
+            <HomeForm />
         </>
     );
 }
