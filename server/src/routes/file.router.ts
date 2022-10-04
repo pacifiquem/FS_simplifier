6d1245e13a275f82069ef343bf9aff1fbed98fd1
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-04 22:58:55 +0200

    download counter route added

--- server/src/routes/file.router.ts
+++ server/src/routes/file.router.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 const router:express.Router = express.Router();
 import multer from 'multer';
-import { home,  uploadFile, downloadFile } from '../controller/file.controller';
+import { home,  uploadFile, downloadFile, downloadCounter } from '../controller/file.controller';
 import genIdentifier from '../middleware/identifierGen.middleware';
 
 
@@ -11,5 +11,6 @@ const upload = multer({dest: 'uploads/'});
 router.route('/').get(home);
 router.route('/file').post( upload.single("uploadedFile") , genIdentifier,  uploadFile );
 router.route('/file/download/:identifier').post(downloadFile).get(downloadFile);
+router.route('/downloadCounter').get(downloadCounter);
 
 export default router;
