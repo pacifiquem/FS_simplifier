c4b037658b2190e8bd33a76cf44834091247d685
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-04 23:14:07 +0200

    routes

--- server/src/routes/file.router.ts
+++ server/src/routes/file.router.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 const router:express.Router = express.Router();
 import multer from 'multer';
-import { home,  uploadFile, downloadFile, downloadCounter } from '../controller/file.controller';
+import { home,  uploadFile, downloadFile, downloadCounter, contactHandler } from '../controller/file.controller';
 import genIdentifier from '../middleware/identifierGen.middleware';
 
 
@@ -12,5 +12,6 @@ router.route('/').get(home);
 router.route('/file').post( upload.single("uploadedFile") , genIdentifier,  uploadFile );
 router.route('/file/download/:identifier').post(downloadFile).get(downloadFile);
 router.route('/downloadCounter').get(downloadCounter);
+router.route('/contactus').post(contactHandler);
 
 export default router;
