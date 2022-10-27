da32c067a0c2929d13bc21c1b95ae3cbaff36800
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-27 21:50:18 +0200

    [feat] : google-font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        libre: ['"Lato"', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: [],
 };
