3b3137149faa74141579e7ddf97649074ce457dc
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-11-01 21:23:45 +0200

    [feat] : copyright component added

---
+++ client/src/components/copyright.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+  name: "CopyRight",
+};
+</script>
+
+<template>
+  <p class="flex flex-row justify-center items-center">
+    <a
+      href="https://github.com/pacifiquem"
+      class="text-center absolute bottom-3 text-[1rem] text-textColor"
+      >pacifiquem@github.com</a
+    >
+  </p>
+</template>
