f19e09dc5011dc752a4e086898e1a1ed6d933942
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-11-01 21:25:10 +0200

    [refractor] : modifiecation to nav

--- client/src/components/nav.vue
+++ client/src/components/nav.vue
@@ -6,20 +6,22 @@ export default {
 
 <template>
   <div
-    class="flex flex-row gap-[3%] items-center py-2 bg-[#feffff] shadow-md w-[100vw] h-[10vh] text-[#292929e3]"
+    class="flex flex-row gap-[2%] items-center py-2 bg-[#feffff] shadow-md w-[100vw] h-[10vh] text-[#292929e3]"
   >
     <img src="../assets/logo.png" alt="FS.simplifier LOGO" class="ml-[7.5%]" />
     <div class="flex flex-row gap-12 p-4 text-[1.1rem]">
-      <button class="hover:text-[#3232e8ec] hover:text-[1.13rem]">
-        Download Counter
-      </button>
-      <button class="hover:text-[#3232e8ec] hover:text-[1.13rem]">Usage</button>
-      <button class="hover:text-[#3232e8ec] hover:text-[1.13rem]">
+      <button class="hover:text-[#3232e8ec] hover:text-[1.11rem]">Usage</button>
+      <button class="hover:text-[#3232e8ec] hover:text-[1.11rem]">About</button>
+      <button class="hover:text-[#3232e8ec] hover:text-[1.11rem]">
         Contact
       </button>
-      <button class="hover:text-[#3232e8ec] hover:text-[1.13rem]">About</button>
+      <button class="hover:text-[#3232e8ec] hover:text-[1.11rem]">
+        Download Counter
+      </button>
     </div>
-    <button class="bg-[#3232e8ec] ml-[30%] rounded h-[60%] w-[7%]">
+    <button
+      class="bg-[#3232e8ec] text-white ml-[30%] rounded h-[55%] w-[7%] active:scale-[98%]"
+    >
       Donate
     </button>
   </div>
