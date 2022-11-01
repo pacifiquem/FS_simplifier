bfcd60be0cc3b72bbce5420dc6adafd850bc70d8
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-11-01 21:26:17 +0200

    [feat]: home.vue added

---
+++ client/src/views/home.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import NavBar from "../components/nav.vue";
+import FormComponent from "../components/form-component.vue";
+import CopyRight from "../components/copyright.vue";
+
+export default {
+  name: "HomePage",
+  components: {
+    NavBar: NavBar,
+    FormComponent: FormComponent,
+    CopyRight: CopyRight,
+  },
+};
+</script>
+
+<template>
+  <NavBar />
+  <div class="bg-[#f7f7f7] relative">
+    <FormComponent />
+    <CopyRight />
+  </div>
+</template>
+
+<style scoped></style>
