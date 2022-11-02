c02d112dbf4f78546e340b6b56b376714d6edc08
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-11-02 21:47:25 +0200

    [feat] : contact.vue's features added

--- client/src/components/contact.vue
+++ client/src/components/contact.vue
@@ -9,10 +9,37 @@ export default {
 </script>
 
 <template>
-  <div
-    class="w-[100%] h-[100%] bg-[#f7f7f7] flex flex-row justify-center items-center"
-  >
-    <div class="h-[80%] w-[70%] shadow bg-[#feffff] mt-[-4.5rem]"></div>
+  <div class="h-[90vh] w-[100vw] flex flex-col justify-center items-center">
+    <div class="h-[70%] w-[35%] shadow bg-[#feffff] mt-[-4.5rem]">
+      <form
+        action="#"
+        method="POST"
+        class="w-[100%] h-[100%] p-6 flex flex-col items-center"
+      >
+        <h2 class="text-[#232323db] text-3xl font-medium">Contact Us</h2>
+        <div class="ml-1 w-[85%] h-[12%] mt-8 mb-6">
+          <input
+            type="password"
+            class="w-[100%] h-[100%] outline-[.5px] border-[#2a2a2b78] border-solid border-[1px] rounded"
+            name="message"
+            id="MessageInput"
+            placeholder="Enter Your Names"
+          />
+        </div>
+        <div class="ml-1 w-[85%] h-[40%]">
+          <textarea
+            class="w-[100%] h-[100%] border-[#2a2a2b78] border-solid border-2 rounded resize-none"
+          >
+          </textarea>
+        </div>
+        <button
+          type="submit"
+          class="bg-[#3232e8ec] w-[85%] h-[12%] text-white text-[1rem] rounded mt-7 active:scale[98%] hover:cursor-pointer"
+        >
+          Send Message
+        </button>
+      </form>
+    </div>
   </div>
   <CopyrightComponent />
 </template>
