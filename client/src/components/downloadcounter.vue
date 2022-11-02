3d8838c4bc679a33ffc1cf36774eff8fd3575aa1
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-11-02 09:30:27 +0200

    [feat] : downloadcounter.vue added

---
+++ client/src/components/downloadcounter.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import CopyrightComponent from './copyright.vue';
+export default {
+  name: "DownloadCounterComponent",
+  components: {
+    CopyrightComponent: CopyrightComponent
+  }
+};
+</script>
+
+<template>
+  <div
+    class="w-[100%] h-[100%] bg-[#f7f7f7] flex flex-row justify-center items-center"
+  >
+    <div class="h-[80%] w-[70%] shadow bg-[#feffff] mt-[-4.5rem]"></div>
+  </div>
+  <CopyrightComponent />
+</template>
+
+<style scoped></style>
