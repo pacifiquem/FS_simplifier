15be1ec1bd0d71536e1cd2fae25e97cac809507c
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-11-02 09:31:21 +0200

    [feat] : usage.vue integrated

--- client/src/components/nav.vue
+++ client/src/components/nav.vue
@@ -1,6 +1,28 @@
 <script lang="ts">
+import UsageComponent from "./Usage.vue";
 export default {
   name: "NavBar",
+  components: {
+    UsageComponent: UsageComponent,
+  },
+  data: function () {
+    return {
+      lina: "notDisplayed",
+      below: "above",
+    };
+  },
+  methods: {
+    toogleUsage() {
+      if (this.lina === "notDisplayed") {
+        this.lina = "displayed";
+      } else {
+        this.lina = "notDisplayed";
+      }
+    },
+    imagesToogleUsage() {
+      this.lina = "notDisplayed";
+    }
+  },
 };
 </script>
 
@@ -8,9 +30,19 @@ export default {
   <div
     class="flex flex-row gap-[2%] items-center py-2 bg-[#feffff] shadow-md w-[100vw] h-[10vh] text-[#292929e3]"
   >
-    <img src="../assets/logo.png" alt="FS.simplifier LOGO" class="ml-[7.5%]" />
+    <img
+      src="../assets/logo.png"
+      alt="FS.simplifier LOGO"
+      class="ml-[7.5%] hover:cursor-pointer"
+      v-on:click="imagesToogleUsage"
+    />
     <div class="flex flex-row gap-12 p-4 text-[1.1rem]">
-      <button class="hover:text-[#3232e8ec] hover:text-[1.11rem]">Usage</button>
+      <button
+        class="hover:text-[#3232e8ec] hover:text-[1.11rem]"
+        v-on:click="toogleUsage"
+      >
+        Usage
+      </button>
       <button class="hover:text-[#3232e8ec] hover:text-[1.11rem]">About</button>
       <button class="hover:text-[#3232e8ec] hover:text-[1.11rem]">
         Contact
@@ -25,6 +57,20 @@ export default {
       Donate
     </button>
   </div>
+  <div :class="lina">
+    <UsageComponent />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.displayed {
+  display: block;
+  position: absolute;
+  height: 90vh;
+  width: 100vw;
+  z-index: 1;
+}
+.notDisplayed {
+  display: none;
+}
+</style>
